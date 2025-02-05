9c965ed475e491a2a2b6c431af89376962e98549
정승주 <ilovesecu@gmail.com>
2025-02-05 15:55:34 +0900

    -메시지 전송 작업 (MongoDB 비동기 저장, STOMP 전송)

--- src/main/java/com/example/redisinspring/chat/controller/ChatController.java
+++ src/main/java/com/example/redisinspring/chat/controller/ChatController.java
@@ -48,9 +48,12 @@ public class ChatController {
     }
 
     @MessageMapping("/send")
-    public ResponseEntity<Void> sendMessage(@Payload ChatMessage message){
-        // 메시지를 해당 채팅방 구독자들에게 전송
-        messagingTemplate.convertAndSend("/sub/room/"+message.getRoomId(), message);
-        return ResponseEntity.ok().build();
+    public Mono<ResponseEntity<Void>> sendMessage(@Payload ChatMessage message){
+        //메시지 MongoDB에 저장
+        return chatMessageService.saveMessage(message.getRoomId(), message.getContent(), message.getWriterId()).flatMap(savedMsg -> {
+            // 메시지를 해당 채팅방 구독자들에게 전송
+            messagingTemplate.convertAndSend("/sub/room/"+savedMsg.getRoomId(), savedMsg);
+            return Mono.just(ResponseEntity.ok().build());
+        });
     }
 }
